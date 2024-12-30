e771c7d1c2333f3010500b3408836c46a36b2188
jinwangong <gongjinwang@live.com>
2024-12-30 16:58:43 +0800

    Update IP.js

--- IPx.js
+++ IPx.js
@@ -8,7 +8,7 @@ var title = obj['country'];
 var subtitle = obj['region'] + ' ' + obj['city'] + ' ' + obj['isp'];
 var ip = obj['query'];
 var description = "国家" + ":" + obj['country'] + '\n' + 
-                  "洲" + ":" + obj['region'] + '\n' + 
+                  "洲" + ":" + obj['regionName'] + '\n' + 
                   "城市" + ":" + obj['city'] + '\n' + 
                   "运营商" + ":" + obj['isp'] + '\n' + 
                   "数据中心" + ":" + obj['org'];
