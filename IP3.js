2ae84a38f871ab467d7b218594e3345eebab5bd9
jinwangong <gongjinwang@live.com>
2024-12-30 17:43:06 +0800

    更新 IP4.js

--- IP3.js
+++ IP3.js
@@ -4,14 +4,19 @@ if ($response.statusCode != 200) {
 
 var body = $response.body;
 var obj = JSON.parse(body);
+
+function hasChinese(str) {
+  return /[\u4e00-\u9fa5]/.test(str);
+}
+
 var title = obj['country'];
-var subtitle = obj['regionName'] + ' ' + obj['city'] + ' ' + obj['isp'];
+var regionName = hasChinese(obj['regionName']) ? obj['regionName'] : ""; 
+var subtitle = regionName + ' ' + obj['city'] + ' ' + obj['isp'];
 var ip = obj['query'];
 var description = "国家" + ":" + obj['country'] + '\n' + 
-                  "洲" + ":" + obj['regionName'] + '\n' + 
+                  "洲" + ":" + regionName + '\n' + 
                   "城市" + ":" + obj['city'] + '\n' + 
                   "运营商" + ":" + obj['isp'] + '\n' + 
                   "数据中心" + ":" + obj['org'];
 
 $done({title, subtitle, ip, description});
-
