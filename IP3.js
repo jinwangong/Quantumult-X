41342350df752147283f2d0d2176586eb261481b
jinwangong <gongjinwang@live.com>
2024-12-30 17:05:12 +0800

    Update and rename IP2.js to IP3.js

--- IP3.js
+++ IP3.js
@@ -5,7 +5,7 @@ if ($response.statusCode != 200) {
 var body = $response.body;
 var obj = JSON.parse(body);
 var title = obj['country'];
-var subtitle = obj['region'] + ' ' + obj['city'] + ' ' + obj['isp'];
+var subtitle = obj['regionName'] + ' ' + obj['city'] + ' ' + obj['isp'];
 var ip = obj['query'];
 var description = "国家" + ":" + obj['country'] + '\n' + 
                   "洲" + ":" + obj['regionName'] + '\n' + 
